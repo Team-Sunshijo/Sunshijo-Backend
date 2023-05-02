e343e91b1fa2288919e458925f4f03b3da9a72e8
woonseok105 <yws3786@gmail.com>
2023-05-03 00:42:27 +0900

    authDetails

---
+++ sunshijo-infrastructure/src/main/kotlin/com/sunshijo/global/security/auth/AuthDetails.kt
@@ -0,0 +1,24 @@
+package com.sunshijo.global.security.auth
+
+import com.sunshijo.domain.teacher.persistence.entity.TeacherEntity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails (
+        private val user: TeacherEntity
+) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = user.accountId
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
---
+++ sunshijo-infrastructure/src/main/kotlin/com/sunshijo/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,19 @@
+package com.sunshijo.global.security.auth
+
+import com.sunshijo.domain.teacher.persistence.TeacherRepository
+import com.sunshijo.global.exception.UserNotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import java.util.UUID
+
+class AuthDetailsService (
+        private val teacherRepository: TeacherRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user = teacherRepository.findByIdOrNull(UUID.fromString(username))
+                ?: throw UserNotFoundException
+        return AuthDetails(user)
+    }
+}
