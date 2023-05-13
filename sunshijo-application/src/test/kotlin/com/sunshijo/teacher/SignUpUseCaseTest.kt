d9773dd57008ec6e64fc0b2ced54fc846b44e722
woonseok105 <yws3786@gmail.com>
2023-05-14 03:41:12 +0900

    SignUpUseCase Test

---
+++ sunshijo-application/src/test/kotlin/com/sunshijo/teacher/SignUpUseCaseTest.kt
@@ -0,0 +1,64 @@
+package com.sunshijo.teacher
+
+import com.sunshijo.domain.teacher.api.dto.request.DomainSignUpRequest
+import com.sunshijo.domain.teacher.domain.Teacher
+import com.sunshijo.domain.teacher.exception.SignUpCodeMismatchException
+import com.sunshijo.domain.teacher.spi.CommandUserPort
+import com.sunshijo.domain.teacher.spi.UserSecurityPort
+import com.sunshijo.domain.teacher.usecase.SignUpUseCase
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+
+internal class SignUpUseCaseTest : DescribeSpec({
+
+    val commandUserPort: CommandUserPort = mockk()
+    val userSecurityPort: UserSecurityPort = mockk()
+
+    val signUpUseCase = SignUpUseCase(commandUserPort, userSecurityPort)
+
+    val request: DomainSignUpRequest = mockk()
+
+    describe("SignUpUseCase") {
+        val accountId = "testId"
+        val password = "testPassword"
+        val name = "testName"
+        val signUpCode = "DsmTeacher"
+
+        context("잘못된 인증코드") {
+            every { request.accountId } returns accountId
+            every { request.password } returns password
+            every { request.name } returns name
+            every { request.signUpCode } returns "incorrectCode"
+
+            it("SignUpCodeMismatchException이 발생해야 합니다") {
+                val exception = shouldThrow<SignUpCodeMismatchException> {
+                    signUpUseCase.signUp(request)
+                }
+                exception.message shouldBe "SignUp Code Mis Match"
+            }
+        }
+
+        context("올바른 인증코드") {
+            every { request.accountId } returns accountId
+            every { request.password } returns password
+            every { request.name } returns name
+            every { request.signUpCode } returns signUpCode
+            every { userSecurityPort.encodePassword(any()) } returns "encodedPassword"
+            every { commandUserPort.saveUser(any()) } just Runs
+
+            it("사용자가 저장되어야 합니다") {
+                signUpUseCase.signUp(request)
+                commandUserPort.saveUser(Teacher(
+                        accountId = accountId,
+                        password = userSecurityPort.encodePassword(password),
+                        name = name
+                ))
+            }
+        }
+    }
+})
