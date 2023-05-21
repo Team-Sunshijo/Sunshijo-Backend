2bde1ce6fe1c1175bc665fb73ff7e0c6628c738a
woonseok105 <yws3786@gmail.com>
2023-05-22 08:36:27 +0900

    domain modify

--- sunshijo-application/src/main/kotlin/com/sunshijo/domain/timetable/domain/TimeTable.kt
+++ sunshijo-application/src/main/kotlin/com/sunshijo/domain/timetable/domain/TimeTable.kt
@@ -1,7 +1,6 @@
 package com.sunshijo.domain.timetable.domain
 
 import com.sunshijo.annotation.Aggregate
-import com.sunshijo.domain.subject.domain.Subject
 import com.sunshijo.domain.teacher.domain.Teacher
 import java.util.Date
 
@@ -18,7 +17,7 @@ class TimeTable (
 
         val weekOfDate: Date,
 
-        val teacher: Teacher,
+        val subject: String,
 
-        val subject: Subject
+        val teacher: Teacher
 )
