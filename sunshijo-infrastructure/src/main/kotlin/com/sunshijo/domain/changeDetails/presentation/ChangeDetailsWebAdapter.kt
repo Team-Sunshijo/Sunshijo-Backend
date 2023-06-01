847a78991603ecdc516e6d11415d9b9e93435e93
woonseok105 <yws3786@gmail.com>
2023-06-01 10:15:26 +0900

    add Http status

--- sunshijo-infrastructure/src/main/kotlin/com/sunshijo/domain/changeDetails/presentation/ChangeDetailsWebAdapter.kt
+++ sunshijo-infrastructure/src/main/kotlin/com/sunshijo/domain/changeDetails/presentation/ChangeDetailsWebAdapter.kt
@@ -4,11 +4,13 @@ import com.sunshijo.domain.changeDetails.api.CreateChangeDetailsPort
 import com.sunshijo.domain.changeDetails.api.QueryChangeDetailsListPort
 import com.sunshijo.domain.changeDetails.api.dto.request.ChangeDetailsRequest
 import com.sunshijo.domain.changeDetails.api.dto.response.QueryChangeDetailsListResponse
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/timetable/changeDetails")
@@ -23,6 +25,7 @@ class ChangeDetailsWebAdapter(
         return queryChangeDetailsListPort.execute(grade, classNum)
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/request")
     fun createChangeDetails(@RequestBody request: ChangeDetailsRequest) {
         createChangeDetailsPort.execute(request)
